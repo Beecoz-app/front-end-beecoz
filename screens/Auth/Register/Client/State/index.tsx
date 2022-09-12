dcc2d619a8ed7be8699574a05c3634b07ef2f500
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-12 19:13:11 -0300

    build: remove react-native-input-select

--- screens/Auth/Register/Client/State/index.tsx
+++ screens/Auth/Register/Client/State/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native"
+import { Text, View } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { AppGenericButton } from "../../../../../components/AppComponents/Buttons/Generic"
+import { AppGenericButton } from "../../../../../components/AppComponents/Buttons/Generic";
 import { AuthStackParams } from "../../../../../navigation/Auth/AuthStackNavigator";
-import {useTheme} from 'styled-components'
-import {ButtonContainer, Container, DataContainer, Title} from './styles'
+import { useTheme } from "styled-components";
+import Dropdown from "react-native-input-select";
+import { ButtonContainer, Container, DataContainer, Title } from "./styles";
 
 type ClientRegisterStateScreenType = NativeStackScreenProps<
   AuthStackParams,
@@ -14,12 +15,39 @@ type ClientRegisterStateScreenType = NativeStackScreenProps<
 export const ClientRegisterStateScreen = ({
   navigation: { navigate },
 }: ClientRegisterStateScreenType) => {
-  const [name, setName] = useState("");
+  const [state, setState] = useState('');
   const theme = useTheme();
+
   return (
     <Container>
       <DataContainer>
         <Title>Tela ainda em processo</Title>
+        <Dropdown
+          label="estados"
+          placeholder="Estado"
+          options={[{ name: "São Paulo", code: "SP" }]}
+          optionLabel={"name"}
+          optionValue={"code"}
+          selectedValue={state}
+          onValueChange={(value: string) => setState(value)}
+          primaryColor={"green"}
+          disabled={false}
+          dropdownContainerStyle={{}}
+          dropdownErrorStyle={{}}
+          dropdownErrorTextStyle={{}}
+          dropdownHelperTextStyle={{}}
+          dropdownStyle={{}}
+          error={'a'}
+          helperText={'a'}
+          isMultiple={false}
+          isSearchable={false}
+          labelStyle={'a'}
+          modalBackgroundStyle={{}}
+          modalOptionsContainer={{}}
+          multipleSelectedItemStyle={{}}
+          searchInputStyle={{}}
+          selectedItemStyle={{}}
+        />
       </DataContainer>
       <ButtonContainer>
         <AppGenericButton
