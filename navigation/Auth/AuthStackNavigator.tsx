bd7f6c8fedf6892c949961619f128c6b9c7441d0
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-12 18:04:58 -0300

    feat: add insert client photo stack routes

--- navigation/Auth/AuthStackNavigator.tsx
+++ navigation/Auth/AuthStackNavigator.tsx
@@ -21,6 +21,9 @@ export type AuthStackParams = {
   registerClientState: undefined;
   registerClientCPF: undefined;
 
+  insertCLientRGPhoto: undefined;
+  insertCLientProofPhoto: undefined;
+  insertCLientPersonalPhoto: undefined;
 
 };
 
--- screens/Auth/Register/Client/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "../../../../styles/styledComponents";
-
-export const Container = styled.View`
-  flex: 1;
-
-  align-items: center;
-
-  background-color: ${({ theme }) => theme.colors.background};
-`;
