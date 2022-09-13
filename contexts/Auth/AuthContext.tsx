8dbd614e7ae6a4afe472007b3592b611afca8acc
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-13 14:03:24 -0300

    build: add axios

--- contexts/Auth/AuthContext.tsx
+++ contexts/Auth/AuthContext.tsx
@@ -5,35 +5,21 @@ import { IClient } from "../../interfaces/User/CLient/IClient";
 export interface IAuthContext {
   user: IClient | IAutonomous | null;
   setUser:
-    | React.Dispatch<React.SetStateAction<IClient>>
-    | React.Dispatch<React.SetStateAction<IAutonomous>>
-    | null;
+    | React.Dispatch<React.SetStateAction<null | IClient | IAutonomous> > | null
 }
 
 interface AuthProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-export const AuthContext = createContext<IAuthContext | null>(null)
+export const AuthContext = createContext<IAuthContext | null>(null);
 
-export const AuthProvider = ({children}: AuthProviderProps) => {
-    const [user, setUser] = useState<IClient>({
-        id: 1,
-        name: "Thiago",
-        birthDate: "04-17-2005",
-        lastName: "Silva",
-        sex: "Male",
-        CPF: "000000",
-        loginId: 1,
-        profileId: 1,
-        typeId: 1,
-        profileImage:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNb1jnC52Zm-Z92rKJuIXRc7ahmsH1mpTUow&usqp=CAU",
-      });
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<IClient | IAutonomous | null>(null);
 
-    return (
-        <AuthContext.Provider value={{user, setUser}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
