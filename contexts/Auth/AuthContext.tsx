735c4105ccfa9c60f2aa7b085689a19b3b854129
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-17 20:46:22 -0300

    feat: add login function in auth context

--- contexts/Auth/AuthContext.tsx
+++ contexts/Auth/AuthContext.tsx
@@ -1,11 +1,13 @@
 import { createContext, ReactNode, useState } from "react";
 import { IAutonomous } from "../../interfaces/User/Autonomous/IAutonomous";
 import { IClient } from "../../interfaces/User/CLient/IClient";
+import { api } from "../../services/api";
 
 export interface IAuthContext {
   user: IClient | IAutonomous | null;
   setUser:
-    | React.Dispatch<React.SetStateAction<null | IClient | IAutonomous> > | null
+    | React.Dispatch<React.SetStateAction<null | IClient | IAutonomous> > | null;
+  login: ({email, password}: {email: string; password: string}) => Promise<void>
 }
 
 interface AuthProviderProps {
@@ -17,8 +19,21 @@ export const AuthContext = createContext<IAuthContext | null>(null);
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<IClient | IAutonomous | null>(null);
 
+  const login = async ({email, password}: {email: string; password: string}) => {
+    try {
+        const {data} = await api.post('/auth/login', {
+          email,
+          password
+        })
+        
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, login }}>
       {children}
     </AuthContext.Provider>
   );
