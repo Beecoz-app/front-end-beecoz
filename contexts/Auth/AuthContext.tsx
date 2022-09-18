1d88ee61ea759d4b029d283d50680f3ff58b9cd1
drakeira <thiago.p.dasilva2005@gmail.com>
2022-09-18 17:13:42 -0300

    feat: end login function

--- contexts/Auth/AuthContext.tsx
+++ contexts/Auth/AuthContext.tsx
@@ -2,7 +2,8 @@ import axios, { AxiosError } from "axios";
 import { createContext, ReactNode, useState } from "react";
 import { IAutonomous } from "../../interfaces/User/Autonomous/IAutonomous";
 import { IClient } from "../../interfaces/User/CLient/IClient";
-import { api } from "../../services/api";
+import { api, CommonHeaderProperties } from "../../services/api";
+import SecureStore from 'expo-secure-store'
 
 export interface IAuthContext {
   user: IClient | IAutonomous | null;
@@ -22,12 +23,18 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   const handleLogin = async ({email, password}: {email: string; password: string}) => {
     try {
-        const {data} = await api.post('/auth/login', {
+        const {data: {user, token}} = await api.post<{user: IClient | IAutonomous, token: string}>('/auth/login', {
           email,
           password
         })
+
+        setUser(user)
+
+        SecureStore.setItemAsync('user', JSON.stringify(user))
+        SecureStore.setItemAsync('token', `Bearer ${token}`)
+
+        api.defaults.headers = {authorization: `Bearer ${token}`} as CommonHeaderProperties
         
-        console.log('sucess', data)
     } catch (error: unknown) {
         if (axios.isAxiosError(error)) console.log(error)
     }
