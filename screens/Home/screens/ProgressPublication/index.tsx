1e21d80c8b82f245f465cf2b38a8dabf4904265f
drakeira <thiago.p.dasilva2005@gmail.com>
2022-10-22 10:32:31 -0300

    feat: add message if no have publication yet

--- screens/Home/screens/ProgressPublication/index.tsx
+++ screens/Home/screens/ProgressPublication/index.tsx
@@ -1,32 +1,34 @@
 import React, { useContext } from "react";
-import {Text} from 'react-native'
+import { Text, View } from "react-native";
 import { Container, Content, Flat, NoPublicationsMessage } from "./styles";
 import { PostCard } from "../../components/PostCard";
 
-import { IPublicationContext, PublicationContext } from "../../../../contexts/Publication/PublicationContext";
+import {
+  IPublicationContext,
+  PublicationContext,
+} from "../../../../contexts/Publication/PublicationContext";
 
 export const ProgressPosts = () => {
-  const {publications} = useContext(PublicationContext) as IPublicationContext
+  const { publications } = useContext(
+    PublicationContext
+  ) as IPublicationContext;
 
-  console.log(publications)
+  console.log(publications);
 
   return (
     <Container>
       <Content>
         {publications.length > 0 ? (
-
           <Flat
-          data={publications}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => <PostCard data={item} />}
+            data={publications}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({ item }) => <PostCard data={item} />}
           />
-          ): (
-            <NoPublicationsMessage>
-              <Text>
-                AAaaasaaa
-              </Text>
-            </NoPublicationsMessage>
-          )}
+        ) : (
+          <View style={{width: '90%', height: '100%', marginTop: 40}}>
+            <NoPublicationsMessage>Não há pedidos em andamento</NoPublicationsMessage>
+          </View>
+        )}
       </Content>
     </Container>
   );
--- screens/Home/screens/ProgressPublication/styles.ts
+++ screens/Home/screens/ProgressPublication/styles.ts
@@ -21,6 +21,17 @@ export const Flat = (styled.FlatList`
 
 ` as unknown) as typeof FlatList
 
-export const NoPublicationsMessage = styled.View`
-    
+export const NoPublicationsMessage = styled.Text`
+    width: 100%;
+    height: 60px;
+
+    padding: 20px;
+
+    background-color: ${({theme}) => theme.colors.main};
+    color: ${({theme}) => theme.colors.white};
+
+    font-weight: bold;
+
+
+    border-radius: 0%   ;
 `
