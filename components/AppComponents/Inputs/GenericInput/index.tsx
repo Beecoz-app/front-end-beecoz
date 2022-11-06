5d193f2105a2d531bd285245a960924912f20fc1
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-06 20:47:55 -0300

    fix: add text-input-mask

--- components/AppComponents/Inputs/GenericInput/index.tsx
+++ components/AppComponents/Inputs/GenericInput/index.tsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import { StyleSheet, TextInput } from "react-native";
+import TextInputMask from 'react-native-text-input-mask';
 import { setKeyboardType } from "../../../../utils/setKeyboardType";
 
 interface InputProps extends  React.ComponentProps<typeof TextInput>{
   type: "email" | "phone" | "password" | "CPF" | "CNPJ" | 'text';
   placeholder: string;
+  changeText: (text: string) => void,
+  mask: string
 }
 
-export const AppGeneticInput = ({ type, placeholder, ...rest }: InputProps) => {
+export const AppGeneticInput = ({ type, placeholder, changeText, mask,...rest }: InputProps) => {
   const [focusInput, setFocusInput] = useState(false);
 
   return (
-    <TextInput
+    <TextInputMask
       keyboardType={setKeyboardType(type)}
       placeholder={placeholder}
       placeholderTextColor={focusInput ? "#fff" : "#ccc"}
       onFocus={() => setFocusInput(true)}
       onBlur={() => setFocusInput(false)}
+      onChangeText={(formatted, extract) => changeText(String(extract))}
       style={focusInput ? styles.inputFocusStyle : styles.inputStyle}
+      mask={mask}
       {...rest}
     />
   );
