2c8dff272b65887c7debd8364350ddba0ead5896
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-15 07:16:45 -0300

    feat: add modal

--- screens/Client/Home/components/OpenPublicationCard/index.tsx
+++ screens/Client/Home/components/OpenPublicationCard/index.tsx
@@ -1,5 +1,13 @@
 import { useContext, useState } from "react";
-import { Text, View, Image, ListRenderItem, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  ListRenderItem,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
 import { InterestedCard } from "../InterestedCard";
 import { Container, ResumeView } from "./styles";
 import { IPost } from "../../../../../interfaces/Post/IPost";
@@ -8,15 +16,158 @@ import {
   IServiceContext,
   ServiceContext,
 } from "../../../../../contexts/Service/ServiceContext";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { theme } from "../../../../../styles/theme";
+import { AppTextArea } from "../../../../../components/AppComponents/Inputs/TextAreaInput";
+import Icon from "react-native-vector-icons/AntDesign";
+
+const RatingBar = ({
+  maxRating,
+  ratingValue,
+  changeRating,
+}: {
+  maxRating: Array<number>;
+  ratingValue: number;
+  changeRating: (rating: number) => void;
+}) => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+      }}
+    >
+      {maxRating.map((item, index) => (
+        <TouchableOpacity key={item} onPress={() => changeRating(item)}>
+          <Icon
+            name={item <= ratingValue ? "star" : "staro"}
+            style={{ fontSize: 30, color: theme.colors.yellow_p }}
+          />
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
 
 export const OpenPublicationCard = ({ data }: { data: IPost }) => {
   const { serviceTypes } = useContext(ServiceContext) as IServiceContext;
   const [seeInterested, setSeeInterested] = useState(false);
+  const [isModal, setIsModal] = useState(false);
+  const [rating, setRating] = useState(0);
+
+  const handleClosePublication = () => {
+    console.log("aaaaaaaaaa");
+
+    setIsModal(true);
+  };
 
   return (
     <Container seeInterested={seeInterested}>
+      <Modal transparent={false} visible={isModal} animationType="fade">
+        <View
+          style={{
+            flex: 1,
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.colors.background,
+          }}
+        >
+          <View
+            style={{
+              width: "90%",
+              height: "40%",
+              backgroundColor: theme.colors.main,
+              padding: 20,
+              display: "flex",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              borderRadius: 10,
+              position: 'relative'
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                position: 'absolute',
+                padding: 10,
+                alignSelf: 'flex-end',
+                display: "flex",
+                alignContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => setIsModal(false)}
+            >
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "bold",
+                  color: theme.colors.white,
+                }}
+              >
+                X
+              </Text>
+            </TouchableOpacity>
+            <View>
+              <View
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 24,
+                    color: theme.colors.white,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  Avalie esse profissional
+                </Text>
+              </View>
+              <View style={{ width: "100%", height: 100 }}>
+                <RatingBar
+                  maxRating={[1, 2, 3, 4, 5]}
+                  ratingValue={rating}
+                  changeRating={(rating) => setRating(rating)}
+                />
+              </View>
+            </View>
+            <AppTextArea
+              placeholder="envie um comentario"
+              onChange={() => null}
+            />
+            <TouchableOpacity
+              style={{
+                width: "100%",
+                height: 60,
+                backgroundColor: theme.colors.second,
+                borderRadius: 10,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  color: theme.colors.yellow_p,
+                  fontSize: 16,
+                  fontWeight: "bold",
+                }}
+              >
+                Botao
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       <ResumeView>
         <Image
           source={require("../../../../../assets/user.png")}
@@ -50,9 +201,29 @@ export const OpenPublicationCard = ({ data }: { data: IPost }) => {
           </View>
         </View>
       </ResumeView>
-      <View style={{height:40, width: '100%', paddingLeft: 20, paddingRight: 20}}>
-        <TouchableOpacity style={{height: '90%', width: 100, backgroundColor: theme.colors.second, borderRadius: 10, display: 'flex', justifyContent: 'center', alignItems: 'center', alignSelf: 'flex-end'}}>
-          <Text style={{color: theme.colors.yellow_p}}>Concluir Pedido</Text>
+      <View
+        style={{
+          height: 40,
+          width: "100%",
+          paddingLeft: 20,
+          paddingRight: 20,
+          paddingBottom: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            height: "90%",
+            width: 100,
+            backgroundColor: theme.colors.second,
+            borderRadius: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "flex-end",
+          }}
+          onPress={handleClosePublication}
+        >
+          <Text style={{ color: theme.colors.yellow_p }}>Concluir Pedido</Text>
         </TouchableOpacity>
       </View>
     </Container>
