a6bac073043474e0041a44509d1d167fdeb3cd33
drakeira <thiago.p.dasilva2005@gmail.com>
2022-11-15 19:28:15 -0300

    feat: add work context and provider

--- contexts/MainContext.tsx
+++ contexts/MainContext.tsx
@@ -3,15 +3,18 @@ import { AutonomousPublicationProvider } from "./Autonomous/Publication/Autonomo
 import { ChatProvider } from "./Chat/ChatContext";
 import { ClientPublicationProvider } from "./Client/Publication/ClientPublicationContext";
 import { ServiceProvider } from "./Service/ServiceContext";
+import { WorkProvider } from "./Work/WorkContext";
 
 export const MainContextProvider = ({ children }: any) => {
   return (
     <AuthProvider>
       <ClientPublicationProvider>
         <AutonomousPublicationProvider>
-          <ChatProvider>
-            <ServiceProvider>{children}</ServiceProvider>
-          </ChatProvider>
+          <WorkProvider>
+            <ChatProvider>
+              <ServiceProvider>{children}</ServiceProvider>
+            </ChatProvider>
+          </WorkProvider>
         </AutonomousPublicationProvider>
       </ClientPublicationProvider>
     </AuthProvider>
